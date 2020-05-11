a60d2c3546026ce7880604d8c1b998288f1b6009
Oscar Bark <oscar.bark@bitaddict.se>
2020-05-11 18:01:13 +0200

    Add additional test cases

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,4 +1,12 @@
+from os.path import join
+import time
+from time import sleep
+import tempfile
+
 from barktools.base_utils import RingBuffer
+from barktools.base_utils import Clocker
+
+from tests.test_helper import TMP_DIR
 
 class TestRingBuffer:
     
@@ -17,3 +25,41 @@ class TestRingBuffer:
         for item in [1,2,3,4]:
             ring_buffer.put(item)
         assert ring_buffer.last() == 4  
+
+class TestClocker:
+
+    def test_1(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+
+            # Simulate process and make measurements
+            clocker = Clocker(tmp_dir)
+
+            for _ in range(100):
+                clocker.clock('sleep_centi')
+                sleep(0.01)
+                clocker.clock('sleep_milli')
+                sleep(0.001)
+            centi_path = join(tmp_dir, "sleep_centi.txt")
+            milli_path = join(tmp_dir, "sleep_milli.txt")
+            del(clocker) # NOTE: Necessary for tempfile to clean tmp_dir
+            
+            # Assert that measurements are acceptable
+            individual_centi_tol = 5e-2
+            mean_centi_tol = 1e-3
+            centi_times = []
+            with open(centi_path, 'r') as file:
+                for line in file:
+                    centi_times.append(float(line))
+            assert all([abs(centi_time-0.01) < individual_centi_tol for centi_time in centi_times])
+            centi_times_mean = sum(centi_times)/len(centi_times)
+            assert abs(centi_times_mean-0.01) <  mean_centi_tol
+
+            individual_milli_tol = 5e-2
+            mean_milli_tol = 1e-3
+            milli_times = []
+            with open(milli_path, 'r') as file:
+                for line in file:
+                    milli_times.append(float(line))
+            assert all([abs(milli_time-0.001) < individual_milli_tol for milli_time in milli_times])
+            milli_times_mean = sum(milli_times)/len(milli_times)
+            assert abs(milli_times_mean-0.001) <  mean_milli_tol
